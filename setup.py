c0bf63ed98c21570ac6c7180b9c3e67fb5b45634
zhizhuo <zhizhuoshuma@163.com>
2023-08-11 07:19:35 +0800

    修改会原来的名称poc_name，原因安装后无法正常调用

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ import os
 from setuptools import setup, find_packages
 
 setup(
-    name="poc-tool",
+    name="poc_tool",
     license='MIT',
-    version="1.0.4",
+    version="1.0.5",
     description="python Poc 还原原始http请求数据包以及常用工具集成化封装",
     author="zhizhuo",
     author_email="zhizhuoshuma@163.com",
+    url='https://github.com/zhizhuoshuma/poc-tool',
     packages=find_packages(),
     install_requires=[
         "requests",
