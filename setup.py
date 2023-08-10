a683615a7ef34de61c7f146528cfe37fe09a4924
zhizhuo <zhizhuoshuma@163.com>
2023-08-11 07:07:14 +0800

    修改项目名称使其与pip名称一致

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
     name="poc-tool",
     license='MIT',
-    version="1.0.3",
+    version="1.0.4",
     description="python Poc 还原原始http请求数据包以及常用工具集成化封装",
     author="zhizhuo",
     author_email="zhizhuoshuma@163.com",
