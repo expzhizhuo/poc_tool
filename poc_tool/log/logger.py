3020648c62bf90bbdfaeb28f9122d3ba88064c03
zhizhuo <zhizhuoshuma@163.com>
2024-10-15 19:32:30 +0800

    fix 修复依赖bug，解决双层log日志输出问题

--- poc_tool/log/logger.py
+++ poc_tool/log/logger.py
@@ -12,8 +12,6 @@ from colorama import init as wininit
 wininit(autoreset=True)
 
 
-# logging.basicConfig(level=logging.INFO,
-#                     format='[%(asctime)s] %(levelname)s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 class Colored:
     def __init__(self):
         self.Red = '\033[1;31m'  # 红色
@@ -26,25 +24,25 @@ class Colored:
         self.Reset = '\033[0m'  # 终端默认颜色
 
     def red(self, s):
-        return "{0}{1}{2}".format(self.Red, s, self.Reset)
+        return f"{self.Red}{s}{self.Reset}"
 
     def green(self, s):
-        return "{0}{1}{2}".format(self.Green, s, self.Reset)
+        return f"{self.Green}{s}{self.Reset}"
 
     def yellow(self, s):
-        return "{0}{1}{2}".format(self.Yellow, s, self.Reset)
+        return f"{self.Yellow}{s}{self.Reset}"
 
     def blue(self, s):
-        return "{0}{1}{2}".format(self.Blue, s, self.Reset)
+        return f"{self.Blue}{s}{self.Reset}"
 
     def fuchsia(self, s):
-        return "{0}{1}{2}".format(self.Fuchsia, s, self.Reset)
+        return f"{self.Fuchsia}{s}{self.Reset}"
 
     def cyan(self, s):
-        return "{0}{1}{2}".format(self.Cyan, s, self.Reset)
+        return f"{self.Cyan}{s}{self.Reset}"
 
     def white(self, s):
-        return "{0}{1}{2}".format(self.White, s, self.Reset)
+        return f"{self.White}{s}{self.Reset}"
 
 
 color = Colored()
@@ -52,10 +50,10 @@ color = Colored()
 
 class LoggingLevel:
     SUCCESS = 50
-    INFO = 49
-    ERROR = 48
-    WARNING = 47
-    DEBUG = 46
+    INFO = 20
+    ERROR = 40
+    WARNING = 30
+    DEBUG = 10
 
 
 logging.addLevelName(LoggingLevel.SUCCESS, color.cyan("SUCCESS"))
@@ -65,38 +63,41 @@ logging.addLevelName(LoggingLevel.WARNING, color.yellow("WARNING"))
 logging.addLevelName(LoggingLevel.DEBUG, color.blue("DEBUG"))
 
 # 初始化日志
-LOGGER = logging.getLogger()
+LOGGER = logging.getLogger(__name__)
 # 设置输出格式
 formatter = logging.Formatter(
     "%(asctime)s %(levelname)s %(message)s",
     datefmt="[%Y-%m-%d %H:%M:%S]"
 )
-LOGGER_HANDLER = logging.StreamHandler(sys.stdout)
-LOGGER_HANDLER.setFormatter(formatter)
-LOGGER.addHandler(LOGGER_HANDLER)
-LOGGER.setLevel(LoggingLevel.INFO)
+
+# 添加处理器前检查是否已经存在处理器
+if not LOGGER.handlers:
+    LOGGER_HANDLER = logging.StreamHandler(sys.stdout)
+    LOGGER_HANDLER.setFormatter(formatter)
+    LOGGER.addHandler(LOGGER_HANDLER)
+    LOGGER.setLevel(LoggingLevel.INFO)
 
 
 class MY_LOGGER:
     @staticmethod
     def info(msg: str):
-        return LOGGER.log(LoggingLevel.INFO, msg)
+        LOGGER.log(LoggingLevel.INFO, msg)
 
     @staticmethod
     def error(msg: str):
-        return LOGGER.log(LoggingLevel.ERROR, msg)
+        LOGGER.log(LoggingLevel.ERROR, msg)
 
     @staticmethod
     def warning(msg: str):
-        return LOGGER.log(LoggingLevel.WARNING, msg)
+        LOGGER.log(LoggingLevel.WARNING, msg)
 
     @staticmethod
     def debug(msg: str):
-        return LOGGER.log(LoggingLevel.DEBUG, msg)
+        LOGGER.log(LoggingLevel.DEBUG, msg)
 
     @staticmethod
     def success(msg: str):
-        return LOGGER.log(LoggingLevel.SUCCESS, msg)
+        LOGGER.log(LoggingLevel.SUCCESS, msg)
 
 
 log = MY_LOGGER()
